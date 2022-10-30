f3dc775c498ff009700238d3f7639b605c243402
rasulvh <RasulVH@code.local>
2022-10-30 14:57:43 +0400

    all tasks added

---
+++ Generics/Datas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Datas<T>
+    {
+        public T[] _datas;
+
+        public Datas()
+        {
+            _datas = new T[0];
+        }
+
+        public void Add(T data)
+        {
+            Array.Resize(ref _datas, _datas.Length + 1);
+            _datas[_datas.Length - 1] = data;
+        }
+
+        public T[] GetAll()
+        {
+            return _datas;
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Datas<int> datas = new Datas<int>();
+
+            datas.Add(1);
+            datas.Add(2);
+            datas.Add(3);
+            datas.Add(4);
+            datas.Add(5);
+
+            foreach (var item in datas.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
